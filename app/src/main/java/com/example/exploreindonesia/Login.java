1aa86375085cd35719f8504b17637f38ac3eb5fd
ditaprll <ditaprillia1504@gmail.com>
2020-02-17 15:30:16 +0700

    Penambahan pada #10 #11 #13

---
+++ app/src/main/java/com/example/exploreindonesia/Login.java
@@ -0,0 +1,27 @@
+package com.example.exploreindonesia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class Login extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+
+    public void Loginn(View view) {
+    }
+
+    public void LoginFB(View view) {
+    }
+
+    public void LoginGmail(View view) {
+    }
+}
--- app/src/main/java/com/example/exploreindonesia/homepage.java
+++ app/src/main/java/com/example/exploreindonesia/homepage.java
@@ -3,6 +3,7 @@ package com.example.exploreindonesia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class homepage extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void bttnLogin(View view) {
+    }
+
+    public void bttnRegist(View view) {
     }
 }
