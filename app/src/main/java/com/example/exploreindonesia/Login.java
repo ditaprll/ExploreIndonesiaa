f9d1412dcb7085014b0a3876e17a2b74de48d254
ditaprll <ditaprillia1504@gmail.com>
2020-02-18 16:39:16 +0700

    Penambahan/mengganti id, layout pada ref #10 #11

--- app/src/main/java/com/example/exploreindonesia/Login.java
+++ app/src/main/java/com/example/exploreindonesia/Login.java
@@ -23,5 +23,6 @@ public class Login extends AppCompatActivity {
     }
 
     public void LoginGmail(View view) {
+
     }
 }
--- app/src/main/java/com/example/exploreindonesia/Register.java
+++ app/src/main/java/com/example/exploreindonesia/Register.java
@@ -2,6 +2,7 @@ package com.example.exploreindonesia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Register extends AppCompatActivity {
@@ -11,4 +12,7 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    Intent intent = new Intent(this, Login.class);
+    
 }
