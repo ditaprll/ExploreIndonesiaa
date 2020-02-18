36194332840e4266adcfff946ad4f52c051a9220
indriyani0793 <iyani0793@gmail.com>
2020-02-18 16:18:20 +0700

    tambahan onclick log_out di profile_user ref #16

--- app/src/main/java/com/example/exploreindonesia/Profile_User.java
+++ app/src/main/java/com/example/exploreindonesia/Profile_User.java
@@ -3,6 +3,7 @@ package com.example.exploreindonesia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile_User extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Profile_User extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile__user);
+    }
+
+    public void Log_out(View view) {
     }
 }
