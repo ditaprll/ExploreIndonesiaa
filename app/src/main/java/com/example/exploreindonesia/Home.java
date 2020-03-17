39555a96e6b83412780f71f77e2aeddfe75af51b
REFZA_KURNIAWAN <refzakurniawan0@gmail.com>
2020-03-17 11:37:45 +0700

    merubah XML pada home ref#17

--- app/src/main/java/com/example/exploreindonesia/Home.java
+++ app/src/main/java/com/example/exploreindonesia/Home.java
@@ -17,10 +17,9 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_explore, R.id.navigation_wishlist, R.id.navigation_account)
+                R.id.navigation_home, R.id.navigation_wishlist, R.id.navigation_account)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
