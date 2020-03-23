83ec3851893bf7a2f83201ac115bc6f05a9197c7
indriyani0793 <iyani0793@gmail.com>
2020-03-23 23:13:03 +0700

    perubahan xml pada setting ref#34

---
+++ app/src/main/java/com/example/exploreindonesia/settingfragment.java
@@ -0,0 +1,32 @@
+package com.example.exploreindonesia;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class settingfragment extends PreferenceFragmentCompat {
+
+
+
+
+
+    @Override
+    public View onCreatePreferences(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+        return textView;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/exploreindonesia/settingsactivity.java
@@ -0,0 +1,16 @@
+package com.example.exploreindonesia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class settingsactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new settingsfragment())
+                .commit();
+    }
+}
