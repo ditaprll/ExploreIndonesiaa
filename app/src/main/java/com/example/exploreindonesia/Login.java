999eb9e388640cf46525cf7bafde4ae2818567b1
ditaprll <ditaprillia1504@gmail.com>
2020-04-18 17:58:32 +0700

    Penambahan codingan dalam penyambungan ke google ref #48

--- app/src/main/java/com/example/exploreindonesia/Login.java
+++ app/src/main/java/com/example/exploreindonesia/Login.java
@@ -5,9 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.example.exploreindonesia.Login;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
@@ -16,6 +33,10 @@ public class Login extends AppCompatActivity {
     private String pass;
     private String email;
     private FirebaseAuth mAuth;
+    private SignInButton signin;
+    private GoogleSignInClient mGoogle;
+    String TAG = "Login";
+    private int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +44,59 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         password = (EditText) findViewById(R.id.password);
         Email = (EditText) findViewById(R.id.email);
+        signin = findViewById(R.id.loginGmail);
         mAuth = FirebaseAuth.getInstance();
-        
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogle = GoogleSignIn.getClient(this, gso);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                signIn();
+            }
+        });
     }
+
+        private void signIn(){
+            Intent signInIntent = mGoogle.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acct){
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Login.this, "Successfull", Toast.LENGTH_LONG).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                }else{
+                    Toast.makeText(Login.this, "Failed", Toast.LENGTH_LONG).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser user){
+
+    }
+
 
     public void Loginn(View view) {
         pass = password.getText().toString();
@@ -42,7 +113,9 @@ public class Login extends AppCompatActivity {
     }
 
     public void LoginFB(View view) {
-
+        Intent intent = new Intent(this, SubmitReview.class);
+        intent.putExtra("idWisata", "2");
+        startActivity(intent);
     }
 
     public void LoginGmail(View view) {
