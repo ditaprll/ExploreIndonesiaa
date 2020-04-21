2c39f7c103c87118229b4897d81d9b8e489aeec6
ditaprll <ditaprillia1504@gmail.com>
2020-04-21 17:59:01 +0700

    adapter review

---
+++ app/src/androidTest/java/com/example/exploreindonesia/ThankiesReviewTest.java
@@ -0,0 +1,26 @@
+package com.example.exploreindonesia;
+
+import android.widget.ImageView;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ThankiesReviewTest {
+    private ImageView ty;
+
+    @Rule
+    public ActivityTestRule acivityTestRule = new ActivityTestRule<>(ThankiesReview.class);
+
+    private void testGambar(){
+        onView(){
+            
+        }
+    }
+
+    private void onView() {
+    }
+}
